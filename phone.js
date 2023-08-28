a0a9f2ec0395e65c385fd66ea208b556481e0de6
Mehedy-Tanvir <mehedytanvir451@gmail.com>
2023-08-28 15:01:56 +0600

    cards added

---
+++ phone.js
@@ -0,0 +1,39 @@
+const loadPhone = async (searchText) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  );
+  const data = await res.json();
+  const phones = data.data;
+  displayPhones(phones);
+  console.log(phones);
+};
+const displayPhones = (phones) => {
+  const phoneContainer = document.getElementById("phone-container");
+  phoneContainer.textContent = "";
+  phones.forEach((phone) => {
+    console.log(phone);
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
+    phoneCard.innerHTML = ` <figure>
+    <img
+      src="${phone.image}"
+      alt="Shoes"
+    />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${phone.phone_name}</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>`;
+    phoneContainer.appendChild(phoneCard);
+  });
+};
+// handle search button
+const handleSearch = () => {
+  const searchField = document.getElementById("input-search");
+  const searchText = searchField.value;
+  loadPhone(searchText);
+  console.log(searchText);
+};
